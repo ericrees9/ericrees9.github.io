b8df6442dad6ba1f774fe01b6a67c9429def9698
Eric Rees <erees@protonmail.com>
2019-08-21 19:28:14 -0400

    Day before presentation. Updating website.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,74 @@
 console.log("Hello!");
+/*
+// Getting the text on the images to un-hide on hover
+const imgOne = document.getElementById("bodyIMGOne");
+const imgTwo = document.getElementById("bodyIMGTwo");
+const imgThree = document.getElementById("bodyIMGThree");
+const imgFour = document.getElementById("bodyIMGFour");
+const imgFive = document.getElementById("bodyIMGFive");
+const projectText = document.getElementById("textIMG1");
+
+// Adding event listener for all the images in the event of hover
+imgOne.addEventListener("mouseover", presto); 
+imgTwo.addEventListener("mouseover", presto2); 
+imgThree.addEventListener("mouseover", presto3); 
+imgFour.addEventListener("mouseover", presto4); 
+imgFive.addEventListener("mouseover", presto5); 
+
+// Adding event listener for all the images in the event of un-hover
+imgOne.addEventListener("mouseout", chango); 
+imgTwo.addEventListener("mouseout", chango2); 
+imgThree.addEventListener("mouseout", chango3); 
+imgFour.addEventListener("mouseout", chango4); 
+imgFive.addEventListener("mouseout", chango5); 
+
+function presto(){
+    document.getElementById("textIMG1").style.visibility = "visible";
+    return;
+}
+
+function chango(){
+    document.getElementById("textIMG1").style.visibility = "hidden";
+    return;
+}
+
+function presto2(){
+    document.getElementById("textIMG2").style.visibility = "visible";
+    return;
+}
+
+function chango2(){
+    document.getElementById("textIMG2").style.visibility = "hidden";
+    return;
+}
+
+function presto3(){
+    document.getElementById("textIMG3").style.visibility = "visible";
+    return;
+}
+
+function chango3(){
+    document.getElementById("textIMG3").style.visibility = "hidden";
+    return;
+}
+
+function presto4(){
+    document.getElementById("textIMG4").style.visibility = "visible";
+    return;
+}
+
+function chango4(){
+    document.getElementById("textIMG4").style.visibility = "hidden";
+    return;
+}
+
+function presto5(){
+    document.getElementById("textIMG5").style.visibility = "visible";
+    return;
+}
+
+function chango5(){
+    document.getElementById("textIMG5").style.visibility = "hidden";
+    return;
+}
+*/
